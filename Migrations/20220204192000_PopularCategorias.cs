1c3889efa36ebe9a7f6c64fbcbfa02c219eff9cd
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-04 16:31:23 -0300

    Atualização163104022022

---
+++ Migrations/20220204192000_PopularCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaId, CategoriaNome, Descricao) VALUES('Normal','Lanches feito com ingredientes normais')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaId, CategoriaNome, Descricao) VALUES('Natural','Lanches feito com ingredientes integrais e naturais')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
